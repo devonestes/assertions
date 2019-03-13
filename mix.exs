c37bd5c9caedef84434474800e6c2a98e352513d
Devon Estes <devon.c.estes@gmail.com>
2019-03-13 17:19:30 +0100

    make testmetrics an optional test only dep

--- mix.exs
+++ mix.exs
@@ -4,11 +4,11 @@ defmodule Assertions.MixProject do
   def project do
     [
       app: :assertions,
-      version: "0.13.0",
+      version: "0.13.1",
       elixir: "~> 1.7",
       deps: [
-        {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-        {:testmetrics_elixir_client, "~> 1.0"}
+        {:ex_doc, "~> 0.19", only: [:dev], runtime: false},
+        {:testmetrics_elixir_client, "~> 1.0", only: [:test], optional: true}
       ],
       description: description(),
       package: package(),
